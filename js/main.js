1c951c17a563af31fb5fb8ec7763ed09db5d5f26
Fredrik-hansson-nti-johanneberg <119052298+Fredrik-hansson-nti-johanneberg@users.noreply.github.com>
2023-02-24 14:26:41 +0100

    små ändringar

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 
 let myButton = document.querySelector(".meny_knapp")
+
 let nav = document.querySelector("nav")
 
 let lager = document.querySelector(".lager")
+
 lager.addEventListener("click", displayMenu)
+
 myButton.addEventListener("click", displayMenu)
+
 function displayMenu(){
+
     nav.classList.toggle("nav_transition");
+   
     lager.classList.toggle("lager_show");
 };
