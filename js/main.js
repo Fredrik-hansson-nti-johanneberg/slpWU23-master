274f2cd4a687a8094ff9ed30da112d9ea0b5523d
Fredrik-hansson-nti-johanneberg <119052298+Fredrik-hansson-nti-johanneberg@users.noreply.github.com>
2023-03-30 22:17:48 +0200

    sida 4

--- js/main.js
+++ js/main.js
@@ -9,9 +9,40 @@ lager.addEventListener("click", displayMenu)
 
 myButton.addEventListener("click", displayMenu)
 
+
+
+
+
+
+
+
 function displayMenu(){
 
     nav.classList.toggle("nav_transition");
    
     lager.classList.toggle("lager_show");
+};
+
+
+let mindre = document.querySelector(".visa_mindre")
+
+
+mindre.addEventListener("click", visaMindre)
+
+
+
+function visaMindre(){
+
+    if (mindre.innerHTML === "Visa mer"){
+        mindre.innerHTML = "Visa mindre"
+
+
+    }else{
+        mindre.innerHTML = "Visa mer"
+
+    }
+
+
+
+
 };
